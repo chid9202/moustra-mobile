a7670e47171b6d20aa558febb5d6cc73afebe073
daehanchi <chid9202@gmail.com>
2025-09-13 15:43:56 -0500

    refactor animal list with contract

--- lib/services/dtos/animal_dto.dart
+++ lib/services/dtos/animal_dto.dart
@@ -1,15 +1,62 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:moustra/services/dtos/account_dto.dart';
 import 'package:moustra/services/dtos/genotype_dto.dart';
+import 'package:moustra/services/dtos/cage_dto.dart';
 import 'package:moustra/services/dtos/strain_dto.dart';
 
 part 'animal_dto.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class AnimalDto {
+  final int eid;
+  final int animalId;
+  final String animalUuid;
+  final String? physicalTag;
+  final DateTime? dateOfBirth;
+  final String? sex;
+  final List<GenotypeDto>? genotypes;
+  final DateTime? weanDate;
+  final DateTime? endDate;
+  final AccountDto? owner;
+  final CageSummaryDto? cage;
+  final StrainSummaryDto? strain;
+  final String? comment;
+  final DateTime createdDate;
+  final DateTime updatedDate;
+  final AnimalSummaryDto? sire;
+  final List<AnimalSummaryDto>? dam;
+
+  AnimalDto({
+    required this.eid,
+    required this.animalId,
+    required this.animalUuid,
+    this.physicalTag,
+    this.dateOfBirth,
+    this.sex,
+    this.genotypes = const [],
+    this.weanDate,
+    this.endDate,
+    this.owner,
+    this.cage,
+    this.strain,
+    this.comment,
+    required this.createdDate,
+    required this.updatedDate,
+    this.sire,
+    this.dam = const [],
+  });
+
+  factory AnimalDto.fromJson(Map<String, dynamic> json) =>
+      _$AnimalDtoFromJson(json);
+  Map<String, dynamic> toJson() => _$AnimalDtoToJson(this);
+}
 
 @JsonSerializable(explicitToJson: true)
 class AnimalSummaryDto {
   final int animalId;
   final String animalUuid;
   final String? physicalTag;
-  final String? dateOfBirth;
+  final DateTime? dateOfBirth;
   final List<GenotypeDto>? genotypes;
   final int? owner;
   final StrainSummaryDto? strain;
