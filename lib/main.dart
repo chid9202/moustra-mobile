22193141949d0b9a885c399a4a3987a09b161d63
daehanchi <chid9202@gmail.com>
2025-11-02 15:14:27 -0600

    production build

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,12 @@ import 'package:moustra/services/auth_service.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Load environment variables
-  await dotenv.load(fileName: '.env');
+  // Load environment variables; allow override via --dart-define=ENV_FILENAME=<file>
+  const String envFileName = String.fromEnvironment(
+    'ENV_FILENAME',
+    defaultValue: '.env',
+  );
+  await dotenv.load(fileName: envFileName);
 
   await authService.init();
   runApp(const MyApp());
