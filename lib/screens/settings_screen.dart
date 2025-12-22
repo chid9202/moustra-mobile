27124966b112beb38425a5eae70827aa5894e81c
daehanchi <chid9202@gmail.com>
2025-12-22 09:57:54 -0600

    hide subscription tab

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:moustra/constants/account_constants.dart';
+// TODO: Subscription tab temporarily disabled
+// import 'package:moustra/constants/account_constants.dart';
 import 'package:moustra/screens/settings/lab_settings_tab.dart';
-import 'package:moustra/screens/settings/subscription_tab.dart';
+// import 'package:moustra/screens/settings/subscription_tab.dart';
 import 'package:moustra/services/dtos/profile_dto.dart';
 import 'package:moustra/stores/profile_store.dart';
 
@@ -13,8 +14,10 @@ class SettingsScreen extends StatelessWidget {
     return ValueListenableBuilder<ProfileResponseDto?>(
       valueListenable: profileState,
       builder: (context, profile, _) {
-        final isAdmin = profile?.role == AccountRole.admin.value;
-        final tabCount = isAdmin ? 2 : 1;
+        // TODO: Subscription tab temporarily disabled
+        // final isAdmin = profile?.role == AccountRole.admin.value;
+        // final tabCount = isAdmin ? 2 : 1;
+        const tabCount = 1;
 
         return DefaultTabController(
           length: tabCount,
@@ -23,7 +26,8 @@ class SettingsScreen extends StatelessWidget {
               TabBar(
                 tabs: [
                   const Tab(text: 'Lab'),
-                  if (isAdmin) const Tab(text: 'Subscription'),
+                  // TODO: Subscription tab temporarily disabled
+                  // if (isAdmin) const Tab(text: 'Subscription'),
                 ],
               ),
               Expanded(
@@ -31,8 +35,9 @@ class SettingsScreen extends StatelessWidget {
                   children: [
                     // Lab tab content
                     const LabSettingsTab(),
+                    // TODO: Subscription tab temporarily disabled
                     // Subscription tab content - only shown for admins
-                    if (isAdmin) SubscriptionTab(profile: profile),
+                    // if (isAdmin) SubscriptionTab(profile: profile),
                   ],
                 ),
               ),
